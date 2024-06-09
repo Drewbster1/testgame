f28014aa31b4836b9dee774cca88227c02fdfb26
Drewbster1 <alxhdvs@gmail.com>
2024-06-09 01:46:34 -0700

    implement simple movement

--- src/ApplicationPkg/MainApp.cpp
+++ src/ApplicationPkg/MainApp.cpp
@@ -1,6 +1,10 @@
 #include "MainApp.hpp"
 
+#include <cstdint>
+#include <iostream>
+
 namespace app {
+
 void MainApp::Run() {
   window_.setFramerateLimit(kDefaultFrameLimit);
   while (window_.isOpen()) {
@@ -8,11 +12,43 @@ void MainApp::Run() {
     while (window_.pollEvent(event)) {
       if (event.type == sf::Event::Closed) {
         window_.close();
+      } else if (event.type == sf::Event::KeyPressed) {
+        this->Update(event.key.scancode);
       }
     }
+    this->Render();
   }
-  window_.clear();
+}
+
+void MainApp::Render() {
+  // clear old frame
+  window_.clear(sf::Color::Black);
+
+  window_.draw(this->GetGameObject());
+
+  // signal done drawing
   window_.display();
+}
+
+void MainApp::Update(const sf::Keyboard::Scan::Scancode& key_code) {
+  switch (key_code) {
+    case sf::Keyboard::Scan::W:
+      this->GetGameObject().move(0, -10.f);
+      break;
+    case sf::Keyboard::Scan::A:
+      this->GetGameObject().move(-10.f, 0.f);
+      break;
+    case sf::Keyboard::Scan::S:
+      this->GetGameObject().move(0.f, 10.f);
+      break;
+    case sf::Keyboard::Scan::D:
+      this->GetGameObject().move(10.f, 0.f);
+      break;
+    default:
+      std::cout << "idk\n";
+      break;
+  }
+  std::cout << "Position X(" << GetGameObject().getPosition().x << ") Y(" << GetGameObject().getPosition().y << ")\n";
 }
 
 }  // namespace app
--- src/ApplicationPkg/MainApp.hpp
+++ src/ApplicationPkg/MainApp.hpp
@@ -7,7 +7,12 @@ namespace app {
 
 class MainApp {
  public:
-  MainApp() : window_{{kFrameWidth, kFrameHeight}, "test"} {}
+  MainApp()
+      : window_{{kFrameWidth, kFrameHeight}, "test"},
+        game_object_{std::make_unique<sf::RectangleShape>(sf::Vector2{50.f, 50.f})} {
+    game_object_->setFillColor(sf::Color::Blue);
+    game_object_->setPosition(500.f, 500.f);
+  }
   ~MainApp() = default;
 
   MainApp(const MainApp&) = delete;
@@ -17,9 +22,16 @@ class MainApp {
 
   /// @brief Run game
   void Run();
+  /// @brief Render stuff?
+  void Render();
+
+  void Update(const sf::Keyboard::Scan::Scancode& key_code);
+
+  sf::RectangleShape& GetGameObject() { return *game_object_; }
 
  private:
-  sf::RenderWindow window_;
+  sf::RenderWindow                    window_;
+  std::unique_ptr<sf::RectangleShape> game_object_;
 };
 
 }  // namespace app
